3de5b5c311d93a73f5cc83186f9d03ba1c616b4a
aoshea <aoshea@users.noreply.github.com>
2023-01-20 14:56:06 +0000

    1 file modified

--- index.ts
+++ index.ts
@@ -6,6 +6,18 @@ const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
 const test_dict = 'rip ripe prim prime impure premium mire umpire obelisk';
+const longer_list =
+  'https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt';
+const short_list =
+  'https://raw.githubusercontent.com/lor-ethan/Word-Game/master/routes/wordlist.txt';
+fetch(longer_list)
+  .then((res) => res.text())
+  .then((res) => {
+    main_2(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 function make_hashmap(dict) {
   const x = {};
@@ -21,6 +33,21 @@ function make_hashmap(dict) {
   return x;
 }
 
+function make_hashmap_2(dict) {
+  const x = {};
+  const lines = dict.split('\r');
+  for (const line of lines) {
+    const sanitized = line.replace(/[-\n\r]+/g, '');
+    const sorted_index = sanitized.split('').sort().join('');
+    if (x[sorted_index]) {
+      x[sorted_index].push(sanitized);
+    } else {
+      x[sorted_index] = [sanitized];
+    }
+  }
+  return x;
+}
+
 const hashmap = make_hashmap(test_dict);
 
 function remove_char_at_index(x: string, index: number) {
@@ -28,7 +55,7 @@ function remove_char_at_index(x: string, index: number) {
 }
 
 function* find_connecting_words(x, indexed_dict, index) {
-  if (index >= x.length || x.length === 2) {
+  if (index >= x.length || x.length === 3) {
     return false;
   }
   // remove a char from x
@@ -60,8 +87,27 @@ function main(indexed_dict) {
     curr = starters.pop();
     result.push(game_set);
   }
-  console.log(result);
 }
 
-console.log('init');
+function main_2(text) {
+  const hashmap = make_hashmap_2(text);
+  const starters = Object.keys(hashmap).filter((x) => x.length === 7);
+  let curr = starters.pop();
+  let result = [];
+  while (typeof curr !== 'undefined') {
+    const game_set = {
+      root: hashmap[curr],
+      children: [],
+    };
+    const gen = find_connecting_words(curr, hashmap, 0);
+    for (const x of gen) {
+      game_set.children.push(x);
+    }
+    curr = starters.pop();
+    if (game_set.children.length === 4) {
+      result.push(game_set);
+    }
+  }
+}
+
 main(hashmap);
